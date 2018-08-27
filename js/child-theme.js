66548e70bb3fdd9c5eb6db639e2aa9e9d6d1ed57
thebrentc <thebrentc@gmail.com>
2018-08-27 19:09:23 +0100

    Colour tweaks

--- js/child-theme.js
+++ js/child-theme.js
@@ -7854,7 +7854,7 @@ jQuery( document ).ready(function() {
 	var myFullpage = new fullpage('#fullpage', {
         anchors: ['firstPage', 'secondPage', '3rdPage'],
         //sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
-        sectionsColor: ['lightgreen', '#1BBC9B', '#7E8F7C'],
+        sectionsColor: ['#1BBC9B', 'lightgreen', '#7E8F7C'],
         css3: true
     });
 
